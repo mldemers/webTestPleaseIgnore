7c8d773d0035ba43e7df120d2d84689352bb1b99
Logan Demers <logandemers@Logans-MacBook-Pro.local>
2018-05-13 08:48:26 -0400

    Intitial commit

---
+++ src/main/java/com/hozlabs/WebTimes/WebTimesApplication.java
@@ -0,0 +1,12 @@
+package com.hozlabs.WebTimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTimesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hozlabs/WebTimes/WebTimesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hozlabs.WebTimes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebTimesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
